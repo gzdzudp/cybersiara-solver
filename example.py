4bfab7259f4d729c79cdb100d6aba608b8dc79f0
bocian <140558962+gzdzudp@users.noreply.github.com>
2023-08-30 16:01:28 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 from solver import CybersiaraSolver
 
 if __name__ == "__main__":
-    solver = CybersiaraSolver()
+    solver = CybersiaraSolver(url="https://www.cybersiara.com/book-a-demo", masterurlid="OXR2LVNvCuXykkZbB8KZIfh162sNT8S2")
     print(solver.solvecap().get("data"))
